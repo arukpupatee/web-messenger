82dc3a9dee6323d5fa9679140cc0cdb8de1c23f8
Aruk Pupatee <aruk.pupatee@gmail.com>
2018-03-03 20:19:29 +0700

    Implement basic socket.io communication between client and server

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import socketIOClient from "socket.io-client";
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      message: false,
+      endpoint: "http://localhost:5000"
+    };
+  }
+
+  componentDidMount() {
+    const { endpoint } = this.state;
+    const socket = socketIOClient(endpoint);
+    socket.on("new message", data => this.setState({ message: data.message}));
+  }
   render() {
+    const { message } = this.state;
     return (
       <div className="App">
         <header className="App-header">
@@ -12,6 +26,10 @@ class App extends Component {
         <p className="App-intro">
           Web Messenger for FireOneOne Interview
         </p>
+        { message
+          ? <p> { message } </p>
+          : <p> Loading </p>
+        }
       </div>
     );
   }
--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const http = require("http");
 const socketIo = require("socket.io");
 const app = express();
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const api = require("./routes/api");
 app.use('/api', api);
@@ -13,6 +13,11 @@ const io = socketIo(server);
 
 io.on("connection", socket => {
     console.log("New client connected");
+
+    socket.emit('new message', {
+        message: 'Hello User'
+    });
+
     socket.on("disconnect", () => {
         console.log("Client disconnected")
     });
