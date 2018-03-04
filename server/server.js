7d480eba8292e0dd7bf0f9dcc03b575bd566415c
Aruk Pupatee <aruk.pupatee@gmail.com>
2018-03-04 23:20:43 +0700

    Create docker-compose

--- server/server.js
+++ server/server.js
@@ -6,8 +6,23 @@ const mongoose = require('mongoose');
 
 const port = process.env.PORT || 5000;
 
-mongoose.connect('mongodb://localhost/WebMessenger', { config: { autoIndex: false} });
 mongoose.Promise = global.Promise;
+
+const dbOptions = {
+    autoIndex: false,
+    reconnectTries: Number.MAX_VALUE, // Never stop trying to reconnect
+    reconnectInterval: 5000
+}
+//const mongooseUrl = 'mongodb://localhost/WebMessenger'; // for local dev
+const mongooseUrl = 'mongodb://mongodb:27017/WebMessenger'; // for docker
+const connectMongoWithRetry = () => { // reconnect when fail in initial connection
+    mongoose.connect(mongooseUrl, dbOptions)
+        .catch(err => {
+            console.log('Failed to connect to mongo on startup - retrying in 5 sec');
+            setTimeout(connectMongoWithRetry, 5000);
+        });
+}
+connectMongoWithRetry();
 
 const Messages = require('./models/Messages');
 
@@ -18,10 +33,10 @@ const server = http.createServer(app);
 const io = socketIo(server);
 
 io.on("connection", function(socket) {
-    console.log("New client connected");
     var user = null;
 
     socket.on('login', async username => {
+        console.log("New client connected");
         user = username;
         socket.user = user;
         var data = {
